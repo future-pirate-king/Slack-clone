9382b2e9a6c4fe599a9eb9c2faba68c6585be706
future-pirate-king <samd948@gmail.com>
2018-12-24 10:40:50 +0530

    create mutations for team

--- typeDefs/team.js
+++ typeDefs/team.js
@@ -3,8 +3,13 @@ import { gql } from 'apollo-server-express';
 export default gql`
   type Team {
     id: ID!
+    name: String!
     owner: User!
     members: [User!]!
     channels: [Channel!]!
+  }
+
+  type Mutation {
+    createTeam(name: String!): Boolean!
   }
 `;
